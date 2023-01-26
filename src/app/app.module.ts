2ce591f0ae30c5948b14faae44e92df881d514e5
rbrownse <robert.j.brownsell@aexp.com>
2023-01-26 15:57:05 +0000

    feat(admin): added admin component and active tab directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {HeaderComponent} from './components/header/header.component';
+import {ActiveTabDirective} from "./components/header/active-tab.directive";
 import {FeaturedComputerComponent} from './components/featuredComputer/featured-computer.component'
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { SearchResultsComponent } from './components/search-results/search-resul
     HomeComponent,
     HeaderComponent,
     FeaturedComputerComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    ActiveTabDirective,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
