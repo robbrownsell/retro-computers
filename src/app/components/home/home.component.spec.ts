0f2f60fd9acc5a297fa07775c5e5426e342f7244
rbrownse <robert.j.brownsell@aexp.com>
2023-02-03 17:01:47 +0000

    added additional tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
-import { HeaderComponent } from "../header/header.component";
-import { FormsModule } from "@angular/forms";
-import { ActiveTabDirective} from "../header/active-tab.directive";
+import {HomeComponent} from './home.component';
+import {HeaderComponent} from "../header/header.component";
+import {FormsModule} from "@angular/forms";
+import {ActiveTabDirective} from "../header/active-tab.directive";
 import {FeaturedComputerComponent} from "../featuredComputer/featured-computer.component";
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -26,4 +26,17 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a featured computer', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('featured-computer')).not.toBeNull();
+  })
+
+  it('should have a header', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const header = compiled.querySelector('rc-header');
+    expect(header).not.toBeNull();
+    expect(header?.attributes.getNamedItem("ng-reflect-active-component")?.value).toEqual("home");
+  })
+
 });
